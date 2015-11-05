92b098c54deb1ee8f279bce2e51a8c31ba0c620c
jham <jham>
2015-11-05 17:39:33 +0000

    application

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -3,16 +3,40 @@ import scalafx.application.JFXApp
 import scalafx.scene.Scene
 import scalafx.scene.paint.Color
 import scalafx.scene.shape.Rectangle
+import java.util.Scanner
+
 //main application to run and test methods
 object App {
   def main(args: Array[String]) {
-    println("Hello World")
 
-    val Product = new Product
-    Product.GetProducts
-    val Order = new Order
-    Order.GetOrders
-    val PurchaseOrder = new PurchaseOrder
-   // PurchaseOrder.GetPurchaseOrders
+    
+      while (true) {
+
+        println("###################################")
+        println("# Warehouse Order Tracking System #")
+        println("###################################")
+        println("##### Please Choose an option #####")
+        println("###################################")
+        println("#1 Products")
+        println("#2 Orders")
+        println("#3 Change Order Status")
+
+        val scanner = new Scanner(System.in)
+        val line: String = scanner.nextLine()
+        line match {
+          case "1" =>
+            val Products = new Product
+            Products GetProducts
+          case "2" =>
+            val Order = new Order
+            Order GetOrders
+          case "3" =>
+            val Order = new Order
+            Order ChangeOrderStatus
+          case _ => println("choose valid option")
+        }
+      }
+    
+
   }
 }
--- src/main/scala/Order.scala
+++ src/main/scala/Order.scala
@@ -15,7 +15,7 @@ class Order {
     try {
       if (Database.connection == null) {
         Database.connection
-       
+
       }
 
       val statement = Database.connection.createStatement()
@@ -45,7 +45,7 @@ class Order {
       try {
 
         //method for selecting which order you want to look at
-        
+
         val scanner = new Scanner(System.in)
         val OrderLine = new OrderLine
         OrderLine.GetOrderLine(scanner.nextLine().toString())
@@ -63,4 +63,141 @@ class Order {
 
     }
   }
+
+  //method gets all purchase orders from the database
+  def ChangeOrderStatus {
+
+    val Database = new Database
+
+    //if no connection is initiated it will create one
+    try {
+      if (Database.connection == null) {
+        Database.connection
+
+      }
+
+      val scan = new Scanner(System.in)
+      System.out.println("\n Select an Order ID")
+      val id = Integer.parseInt(scan.nextLine())
+
+      val statement = Database.connection.createStatement()
+
+      val sql1 = ("SELECT customerorderid, date, status, employeeid FROM CustomerOrder Where customerorderid = " + id)
+
+      // creates the statement, and run the select query
+      val resultSet1 = statement.executeQuery(sql1)
+
+      //passing gathered column Data into variables
+      while (resultSet1.next()) {
+        val Orderid1 = resultSet1.getString("customerorderid")
+        val date1 = resultSet1.getString("date")
+        val status1 = resultSet1.getString("status")
+        val empid1 = resultSet1.getString("employeeid")
+
+        //print out information gathered from the database
+        println("ID = " + Orderid1 + " Date = " + date1 + " Order Status = " + status1 + " Employee ID = " + empid1)
+        println("\nWould You like to change the Status?")
+        println("#1 Change to Confirmed")
+        println("#2 Change to Processing")
+        println("#3 Change to Dispatched")
+        println("#4 Change to Delivered")
+        println("#5 Exit")
+
+        //scan the next line to choose option
+        val scanner = new Scanner(System.in)
+        val line: String = scanner.nextLine()
+
+        line match {
+          case "1" => Confirmed(Orderid1)
+          case "2" => Processing(Orderid1)
+          case "3" => dispatched(Orderid1)
+          case "4" => Delivered(Orderid1)
+          case _ => println("choose valid option")
+        }
+      }
+      try {
+
+      } catch {
+        case e: Throwable =>
+          e printStackTrace ()
+          println("Failed to select a correct Order ID.");
+
+      }
+      Database.connection close ()
+
+    } catch {
+
+      case e => e.printStackTrace
+
+    }
+  }
+
+  //method to update order status
+  def dispatched(Orderid1: String) {
+    val Database = new Database
+
+    //if no connection is initiated it will create one
+    try {
+      if (Database.connection == null) {
+        Database.connection
+
+      }
+
+      val statement = Database.connection.createStatement()
+      val sql = ("UPDATE CustomerOrder SET status = 'Dispatched' WHERE customerorderid = " + Orderid1)
+      statement.executeUpdate(sql);
+    }
+  }
+
+  //method to update order status
+  def Confirmed(Orderid1: String) {
+    val Database = new Database
+
+    //if no connection is initiated it will create one
+    try {
+      if (Database.connection == null) {
+        Database.connection
+
+      }
+
+      val statement = Database.connection.createStatement()
+      val sql = ("UPDATE CustomerOrder SET status = 'Confirmed' WHERE customerorderid = " + Orderid1)
+      statement.executeUpdate(sql);
+    }
+  }
+
+  //method to update order status
+  def Processing(Orderid1: String) {
+    val Database = new Database
+
+    //if no connection is initiated it will create one
+    try {
+      if (Database.connection == null) {
+        Database.connection
+
+      }
+
+      val statement = Database.connection.createStatement()
+      val sql = ("UPDATE CustomerOrder SET status = 'Processing' WHERE customerorderid = " + Orderid1)
+      statement.executeUpdate(sql);
+    }
+  }
+
+  //method to update order status
+  def Delivered(Orderid1: String) {
+    val Database = new Database
+
+    //if no connection is initiated it will create one
+    try {
+      if (Database.connection == null) {
+        Database.connection
+
+      }
+
+      val statement = Database.connection.createStatement()
+      val sql = ("UPDATE CustomerOrder SET status = 'Delivered' WHERE customerorderid = " + Orderid1)
+      statement.executeUpdate(sql);
+    }
+  }
+
 }
